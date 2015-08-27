2ea048b5a9b57b8cd33e2b83f86fa14ae4fcdc3a
Samuel Bouchet <samuel.bouchet@gmail.com>
2015-08-27 16:13:59 +0200

    release 1.0.1

---
+++ build.bat
@@ -0,0 +1,2 @@
+SET GRADLE_OPTS="-Dfile.encoding=utf-8"
+gradlew build
--- src/main/java/capsule/Main.java
+++ src/main/java/capsule/Main.java
@@ -17,7 +17,7 @@ public class Main {
 
 	public static final String MODID = "capsule";
 	public static final String MODNAME = "Capsule";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	
     public static CreativeTabs tabCapsule = new CapsuleTabs(CreativeTabs.getNextID(), "capsule");
 
