f0f13d2a714244dd07113cbf1e207414b17f4362
Samuel Bouchet <contact@samuel-bouchet.fr>
2022-06-09 23:35:17 +0200

    Fix BlockItem and runtime error cause by example file

--- src/main/java/capsule/blocks/CapsuleBlocks.java
+++ src/main/java/capsule/blocks/CapsuleBlocks.java
@@ -1,6 +1,8 @@
 package capsule.blocks;
 
 import capsule.CapsuleMod;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.api.distmarker.Dist;
@@ -14,14 +16,17 @@ import net.minecraftforge.registries.RegistryObject;
 public class CapsuleBlocks {
 
     private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, CapsuleMod.MODID);
+    private static final DeferredRegister<Item> BLOCKITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, CapsuleMod.MODID);
     private static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES, CapsuleMod.MODID);
 
     public static final RegistryObject<Block> CAPSULE_MARKER = BLOCKS.register("capsulemarker", BlockCapsuleMarker::new);
+    public static final RegistryObject<BlockItem> CAPSULE_MARKER_ITEM = BLOCKITEMS.register("capsulemarker", () -> new BlockItem(CAPSULE_MARKER.get(), new Item.Properties().tab(CapsuleMod.tabCapsule)));
     public static final RegistryObject<BlockEntityType<BlockEntityCapture>> MARKER_TE = BLOCK_ENTITIES.register("capsulemarker_te", () -> BlockEntityType.Builder.of(BlockEntityCapture::new, CAPSULE_MARKER.get()).build(null));
 
     public static void registerBlocks(IEventBus modEventBus) {
         BLOCKS.register(modEventBus);
         BLOCK_ENTITIES.register(modEventBus);
+        BLOCKITEMS.register(modEventBus);
     }
 
     @OnlyIn(Dist.CLIENT)
